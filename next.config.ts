eb0e0e59339cd0e8528c8443fb12fb783da9c718
mebaX <bahamert010@gmail.com>
2026-02-13 01:50:39 +0300

    config.ts changed

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',  // Statik HTML çıktısı (out klasörü) oluşturur
+  images: {
+    unoptimized: true, // Statik export'ta Next.js Image optimizasyonu çalışmaz, bunu kapatmalıyız
+  },
 };
 
 export default nextConfig;
